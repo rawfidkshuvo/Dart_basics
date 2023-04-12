d9024015588b71547c304d3e9fe96b608173d94e
rawfidkshuvo <rawfid.shuvo@outlook.com>
2023-04-12 20:17:31 +0200

    new commit

--- lists.dart
+++ lists.dart
@@ -1,11 +1,21 @@
 void main() {
   //lists
-  var s = [1,2,3];
-  final x = s[0];
+  var s = [1, 2, 3];
+  final x = s[1];
   final l = s.length;
   print('$s');
-  print ('$x');
-  print ('$l');
+  print('$x');
+  print('$l');
+  //add new element
   s.add(4);
+  //remove at index
+  s.removeAt(0);
+  //remove last element
+  s.removeLast();
+  //reverse the list
+  final rev = s.reversed;
+  print(rev.toList());
+  final p = s.first;
   print('$s');
+  print('$p');
 }
