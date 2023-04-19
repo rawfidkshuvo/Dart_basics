c9a9abfa7089a837c783a6ad3072dd2b484a0b95
rawfidkshuvo <rawfid.shuvo@outlook.com>
2023-04-19 20:28:22 +0200

    Const and final.

--- statement.dart
+++ statement.dart
@@ -7,4 +7,10 @@ void main() {
   String msg = 'This is dart';
   print(msg);
   print('The message is: $msg');
+
+  const pi = 3.14;
+  print(pi);
+  final currentTime = DateTime
+      .now(); // not const, because the value is not known at compile time, rather run time.
+  print(currentTime);
 }
